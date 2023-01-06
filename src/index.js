28affb8874c26e7036c6489fbdb7a7da7b0b1d22
quokka-eating-carrots <ovzip7@gmail.com>
2023-01-06 16:28:53 +0900

    Create Component

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ import Blocks from './chapter_15/Blocks';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Blocks />
+    <App />
   </React.StrictMode>
 );
 
